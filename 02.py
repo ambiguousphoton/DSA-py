9559fcaafe4116b8e11efab01a2695da958d16ba
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-11-18 22:31:34 -0800

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,18 @@
+import collections
+stack = collections.deque()
+
+stack.append(65)
+print (stack)
+print (not stack)
+stack.append(45)
+stack.append(63)
+print(stack[-1])
+
+import queue
+stack = queue.LifoQueue(3)
+stack.put(10)
+stack.put(20)
+stack.put(30)
+
+stack.put(5)
+print(stack.get(timeout=1))
