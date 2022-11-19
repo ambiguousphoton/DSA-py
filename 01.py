a91deaf1fff12ea75b23bcec75f0f4db73459298
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-11-18 22:31:21 -0800

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,26 @@
+stack = []
+def push():
+    inp = input("enter a number")
+    stack.append(inp)
+    print(stack)
+
+def pop_elm():
+    if not stack:
+        print("stack is already empty!")
+    else:
+        poped = stack.pop()
+        print(stack ," - ", poped)
+
+def loop():
+    while True:
+        inp = input ("enter any operation - pop , push , quit : ")
+        if inp == "pop":
+            pop_elm()
+        elif inp == "push" :
+            push()
+        elif inp == "quit":
+            break 
+        else:
+            print("wrong input")
+
+loop()
