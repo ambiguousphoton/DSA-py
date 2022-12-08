2760d2cda5e69854aec10fae152ce03184671f15
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-12-08 23:57:24 +0530

    Add files via upload

---
+++ DFS.py
@@ -0,0 +1,27 @@
+# DFS
+from queue import Queue
+counter = 1 
+graph = {}
+while True :
+    val = input("what is " + str(counter) +" st node ? :: ")
+    if val == ".":
+        break
+    my_list = []
+    while True:
+        child = input("child of "+ str(counter) +"node :: ")
+        if child  == ".":
+            
+            break
+        my_list.append(child)
+    graph[val] = my_list
+    counter += 1
+
+s = input ("what is starting node ? :: ")
+visited = set()
+def dfs(visited, g, root):
+    if root not in visited:
+        print (root)
+        visited.add(root)
+        for neighbour in graph[root]:
+            dfs(visited , graph  , neighbour)
+dfs(visited,graph,s)
