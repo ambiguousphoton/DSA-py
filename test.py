4dd047be65accfcfd17bfbe08e08ebab78a98f05
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2023-01-13 23:57:39 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,41 @@
+# arr = [34,53,35,23,53]
+# print(len(arr))
+# a = 34
+# print(a.ref)
+
+# for n in range(len(arr)):
+    
+# #     print(n ," ",arr[n])
+
+# arr = [[0, 0], [1, 5], [3, 0], [4, 0], [5, 0], [5, 5]]
+# a = [4,0]
+# if  a not in arr:
+#     arr.append(a)
+# # print(arr)
+# ab = 5
+# ba = ab 
+# ab+=1
+# print ( id (ab)," ",ab, " ",id(ba)," ",ba)
+
+# a =[0,0,0]
+# print(len(a))
+
+# import stack
+# stack =  stack.stack()
+# stack.pop()
+# stack.push(1)
+# stack.push(2)
+# stack.show()
+
+# class sayhi:
+#     def __english():
+#         print("hi")
+
+#     def english():
+#         print("hi!")
+
+# # sayhi.__english()
+# sayhi.english()  
+# import inspect
+# from queue import Queue
+# print(inspect.getsource(Queue))
